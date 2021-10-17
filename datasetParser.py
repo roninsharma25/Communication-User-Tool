ba3797d615cc564fad96e0b8ac4e7bda99448cde
Ronin Sharma <rrs234@cornell.edu>
2021-10-17 01:29:32 -0400

    added job ad parser and skill extraction script

---
+++ datasetParser.py
@@ -0,0 +1,45 @@
+"""
+
+Takes an input job field and returns the top communication skills required for that job field.
+
+"""
+
+import pandas as pd
+import numpy as np
+import sys
+
+jobAdDataset = "merged_data_final.csv"
+commSkillsMapping = "coded_list_items.csv"
+
+def detectSkills(phrase, skillFields):
+    skills = [i for i in skillFields if isinstance(i, str)]
+    
+    # ADD MORE CASES IF NEEDED
+    if 'write' in phrase or 'written' in phrase:
+        skills += ['written']
+    if 'verbal' in phrase:
+        skills += ['verbal']
+    if 'aural' in phrase:
+        skills += ['aural']
+    
+    return list(set(skills))
+
+if __name__ == "__main__":
+    keyword = sys.argv[1] if len(sys.argv) > 0 else "Computer Hardware Engineer"
+    df = pd.read_csv("data/" + jobAdDataset)
+    df = df[df["Keyword"] == keyword]
+    df2 = pd.read_csv("data/" + commSkillsMapping)
+    allSkills = []
+
+    for des in df["Description"].to_numpy():
+        des = des.lower()
+        for row in df2.to_numpy():
+            phrase = row[2].lower()
+            if phrase in des:
+                skillFields = np.array(row[-4:])
+                skills = detectSkills(phrase, skillFields)
+                allSkills += skills
+
+    dict_ = dict((val, allSkills.count(val)) for val in set(allSkills))
+    sortedDict = sorted(dict_.items(), key = lambda x: x[1], reverse = True)
+    print(sortedDict)
