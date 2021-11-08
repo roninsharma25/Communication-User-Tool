1d2170fcc8ff28dc6322659132b67115219a56de
Danielle <dcs289@cornell.edu>
2021-11-08 10:16:47 -0500

    created page tabs and started skill search page

--- src/App.js
+++ src/App.js
@@ -1,39 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes
+} from 'react-router-dom';
+import SkillSearch from './components/SkillSearch';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   let api = 'https://engrc3350-user-app.herokuapp.com/test/';
   let output;
 
   fetch(api)
-        .then(res => res.json())
-        .then(
-          (result) => {
-            output = result.result;
-          },
-          (error) => {
-            console.log(error);
-          }
-        );
+    .then(res => res.json())
+    .then(
+      (result) => {
+        output = result.result;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          {output}
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <Link to="/" class="nav-link active" aria-current="page">To Home</Link>
+                <Link to="/about" class="nav-link active" aria-current="page">To About</Link>
+                <Link to="/contact" class="nav-link active" aria-current="page">To Contact</Link>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route exact path='/' element={<SkillSearch />}></Route>
+          <Route exact path='/about' element={<About />}></Route>
+          <Route exact path='/contact' element={<Contact />}></Route>
+        </Routes>
+
+        {/* <p>{output}</p> */}
+
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/SkillSearch.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Select from 'react-select';
+
+const options = [
+    { value: 'software_engineer', label: 'Software Engineer' },
+    { value: 'mechanical_engineer', label: 'Mechanical Engineer' },
+    { value: 'data_scientist', label: 'Data Scientist' }
+]
+
+export default class SkillSearch extends Component {
+    state = {
+        selectedOption: null,
+        fade: false
+    }
+
+    handleChange = (selectedOption) => {
+        this.setState({ selectedOption });
+        console.log(this.state.selectedOption);
+        // document.write(`Option selected:`, selectedOption.value); //this prints the selected option
+    }
+
+
+    onClick = () => {
+        console.log(this.state.selectedOption);
+        this.setState({ fade: true })
+    }
+
+    render() {
+        const fade = this.state.fade
+
+        return (
+            <div className="skill-search" >
+                <h1 className="title">Search for jobs you're interested in!</h1>
+                <Select className="searchbar" options={options} isClearable={true} onChange={this.handleChange} />
+                <button type="button" class="btn btn-outline-primary" onClick={this.onClick}
+                    onAnimationEnd={() => this.setState({ fade: false })}
+                    className={fade ? 'fade' : ''}>Search</button>
+            </div>
+        )
+    }
+}
