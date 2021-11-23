e3a6cafa2d8f42290cd54ad0ef38a584d2ab1862
nmadhavani18 <neilmadhavani@gmail.com>
2021-11-23 15:34:40 -0500

    fixed all scripts and updated list of technical skills

--- JAUT/datasetParser.py
+++ JAUT/datasetParser.py
@@ -17,14 +17,20 @@ def detectSkills(phrase, skillFields):
     skills = [i for i in skillFields if isinstance(i, str)]
 
     # ADD MORE CASES IF NEEDED
-    if 'write' in phrase or 'written' in phrase:
-        skills += ['written']
-    if 'verbal' in phrase:
-        skills += ['verbal']
-    if 'aural' in phrase:
-        skills += ['aural']
-    if 'team' in phrase or 'teamwork' in phrase:
-        skills += ['team']
+    if 'team' in phrase or 'teamwork' in phrase or 'teams' in phrase or 'group' in phrase or 'groups' in phrase or 'groupwork' in phrase or 'conflict management' in phrase or 'managing conflict' in phrase or 'feedback' in phrase:
+	       skills +=['teams']
+    if 'empathy' in phrase or 'listening' in phrase or 'active listening' in phrase or 'listening actively' in phrase or 'empathetic listening' in phrase or "point of view" in phrase or 'understanding others' in phrase:
+	       skills +=['listening']
+    if 'writing' in phrase or 'writing skill' in phrase or 'writing ability' in phrase or 'technical writing' in phrase or 'technical document' in phrase or 'presentation' in phrase or 'drafting documents' in phrase or 'creating presentations' in phrase or 'memo writing' in phrase or 'memo creation' in phrase or 'creating reports' in phrase or 'drafting emails' in phrase or 'email drafting' in phrase:
+	       skills +=['technical creation']
+    if 'relationships' in phrase or 'engaging' in phrase or 'small talk' in phrase or 'rapport' in phrase or 'impromptu speaking' in phrase or 'body language' in phrase or 'small talk' in phrase or 'working in diverse group' in phrase:
+	       skills +=['personableness']
+    if 'oral communication' in phrase or 'public speaking' in phrase or 'speaking public' in phrase or 'orally communicating' in phrase or 'verbal communication' in phrase or 'verbal skill' in phrase or 'respect' in phrase or 'confidence' in phrase or 'friendliness' in phrase or 'friendly' in phrase:
+	       skills+=['oral communication']
+    if 'negotiation' in phrase or 'persuasion' in phrase or 'infleunce' in phrase:
+	       skills+=['negotiation']
+    if 'planning' in phrase or 'structuring' in phrase or 'structure' in phrase or 'organizing' in phrase or 'organize' in phrase or 'arrange' in phrase or 'arranging' in phrase:
+	       skills+=['planning']
 
     return list(set(skills))
 
@@ -32,7 +38,7 @@ def parseDataset():
     if len(sys.argv) > 1:
         keyword = sys.argv[1]
     else:
-        keyword = "Computer Hardware Engineer"
+        keyword = "Software Engineer"
     df = pd.read_csv("data/" + jobAdDataset)
     df = df[df["Keyword"] == keyword]
     df2 = pd.read_csv("data/" + commSkillsMapping)
@@ -49,7 +55,7 @@ def parseDataset():
 
     dict_ = dict((val, allSkills.count(val)) for val in set(allSkills) if val != "complicated!")
     sortedDict = sorted(dict_.items(), key = lambda x: x[1], reverse = True)
-    #print("Important Skills For", keyword+"s", "Are:", sortedDict)
+    print("Important Skills For", keyword+"s", "Are:", sortedDict)
 
     return sortedDict
 
--- JAUT/jobAdParser.py
+++ JAUT/jobAdParser.py
@@ -8,16 +8,52 @@ def convert_txt_to_text(path):
     # print(content)
     return content
 
+def checkSkillGroups(word):
+
+    team = {'team', 'teamwork', 'teams', 'group', 'groups', 'groupwork', 'conflict management', 'managing conflict', 'feedback'}
+    listening = {'empathy', 'listening', 'listen', 'active listening', 'listening actively', 'empathetic listening', "point of view", 'understanding others'}
+    technical_creation = {'writing skill', 'writing ability', 'technical writing', 'technical document', 'presentation', 'drafting documents', 'creating presentations', 'memo writing', 'memo creation', 'creating reports', 'drafting emails', 'email drafting'}
+    personableness = {'relationships', 'engaging', 'small talk', 'rapport', 'impromptu speaking', 'body language', 'small talk', 'working in diverse group'}
+    oral_communication = {'oral communication', 'public speaking', 'speaking public', 'orally communicating', 'verbal communication', 'verbal skill', 'respect', 'confidence', 'friendliness', 'friendly'}
+    negotiation = {'negotiation', 'persuasion', 'infleunce'}
+    planning = {'planning', 'structuring', 'structure', 'organizing', 'organize', 'arrange', 'arranging'}
+
+    if word in team:
+        return "team"
+    elif word in listening:
+        return "listening"
+    elif word in technical_creation:
+        return "technical creation"
+    elif word in personableness:
+        return "personableness"
+    elif word in oral_communication:
+        return "oral communication"
+    elif word in negotiation:
+        return "negotiation"
+    elif word in planning:
+        return "planning"
 
 def parseJobAd():
     allSkills = list(map(lambda word: word.lower() if len(word) > 2 else None, convert_txt_to_text("data/exampleJobAd.txt")))
     jobAdWords = set(allSkills)
-    communicationSkills = {"written", "verbal", "aural", "team"}
+    skillsSet = {'team', 'teamwork', 'teams', 'group', 'groups', 'groupwork', 'conflict management', 'managing conflict', 'feedback',
+    'empathy', 'listening', 'listen', 'active listening', 'listening actively', 'empathetic listening', "point of view", 'understanding others',
+    'writing skill', 'writing ability', 'technical writing', 'technical document', 'presentation', 'drafting documents', 'creating presentations',
+    'memo writing', 'memo creation', 'creating reports', 'drafting emails', 'email drafting', 'relationships', 'engaging', 'small talk', 'rapport',
+    'impromptu speaking', 'body language', 'small talk', 'working in diverse group', 'oral communication', 'public speaking', 'speaking public',
+    'orally communicating', 'verbal communication', 'verbal skill', 'respect', 'confidence', 'friendliness', 'friendly', 'negotiation', 'persuasion',
+    'infleunce', 'planning', 'structuring', 'structure', 'organizing', 'organize', 'arrange', 'arranging'}
+    dict_ = {}
+    jobAdWords = jobAdWords.intersection(skillsSet)
 
-    parsedCommunicationSkills = jobAdWords.intersection(communicationSkills)
-    print("Types of Communication Skills Found In Job Ad:", parsedCommunicationSkills)
+    for val in jobAdWords:
+        count = allSkills.count(val)
+        skillGroup = checkSkillGroups(val)
+        if skillGroup in dict_.keys():
+            dict_[skillGroup] += count
+        else:
+            dict_[skillGroup] = count
 
-    dict_ = dict((val, allSkills.count(val)) for val in parsedCommunicationSkills)
     sortedDict = sorted(dict_.items(), key = lambda x: x[1], reverse = True)
     print("# of Times Each Skill Is Mentioned in Job Ad:", sortedDict)
 
