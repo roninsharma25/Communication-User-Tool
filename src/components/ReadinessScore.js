18b8b7ff6f8a904ad0a55ef26266be5f54b4a326
WilliamRockwellEvans <45379622+WilliamRockwellEvans@users.noreply.github.com>
2021-11-29 21:43:39 -0500

    Add files via upload

---
+++ src/components/ReadinessScore.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export class ReadinessScore extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Your Readiness Score: </h4>
+      </div>
+    )
+  }
+}
+
+export default ReadinessScore
