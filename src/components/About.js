d2091f1d772a28fdeade8152db4ecc4b67edb949
Danielle <dcs289@cornell.edu>
2021-12-01 23:26:08 -0500

    re-added updated files

--- src/components/About.js
+++ src/components/About.js
@@ -1,28 +1,71 @@
+// import React from 'react';
+// import ds_headshot from '../Danielle_Simon_Headshot.png';
+// import Member from './Member';
+
+// function About() {
+//     return <div>
+//         <p>TESTING ABOUT US PAGE</p>
+//         <div className="team_members">
+//             <div className="row">
+//                 <Member name="Allison Hutchison" pic={ds_headshot} />
+//                 <Member name="Danielle Simon" pic={ds_headshot} />
+//                 <Member name="Nina Xie" pic={ds_headshot} />
+//                 <Member name="William Evans" pic={ds_headshot} />
+//                 {/* <img src={ds_headshot} alt="Allison Hutchison's Headshot" className="headshots" />
+//                 <img src={ds_headshot} alt="Danielle Simon's Headshot" className="headshots" />
+//                 <img src={ds_headshot} alt="Nina Xie's Headshot" className="headshots" />
+//                 <img src={ds_headshot} alt="William Evans's Headshot" className="headshots" /> */}
+//             </div>
+//             <div className="row">
+//                 <Member name="Ronin Sharma" pic={ds_headshot} />
+//                 <Member name="Neil Madhavani" pic={ds_headshot} />
+//                 <Member name="Paul Beck" pic={ds_headshot} />
+//                 {/* <img src={ds_headshot} alt="Ronin Sharma's Headshot" className="headshots" />
+//                 <img src={ds_headshot} alt="Neil Madhavani's Headshot" className="headshots" />
+//                 <img src={ds_headshot} alt="Paul Beck's Headshot" className="headshots" /> */}
+//             </div>
+//         </div>
+//     </div>
+// }
+
+// export default About;
+
 import React from 'react';
+import ah_headshot from '../Allison_Hutchison_Headshot.png';
 import ds_headshot from '../Danielle_Simon_Headshot.png';
+import nx_headshot from '../Nina_Headshot.png';
+import we_headshot from '../Will_Headshot.jpeg';
+import rs_headshot from '../Ronin_Headshot.jpg';
+import nm_headshot from '../Neil_Headshot.jpg';
+import pb_headshot from '../Paul_Headshot.jpg';
 import Member from './Member';
 
 function About() {
+
+    const ah_bio = "I earned a PhD from the Rhetoric & Writing program at Virginia Tech. I love technical and engineering communication, writing centers, and music. I am an experienced educator in both online and face-to-face settings with a strong passion for student engagement and success. My pedagogical axiom is, 'If we're not having fun, we're not doing it right.'";
+    const ds_bio = "I am currently a senior studying Computer Science in the College of Engineering at Cornell. I am pursuing a career in software engineering, and will be working as a full-time Software Developer after graduating in May 2022. I am interested in full stack development, and have prior experience in the field from past internships, clubs, and classes. Outside of school, I enjoy playing soccer and practicing juggling!"
+    const nx_bio = "I’m a senior majoring in Information Science in the College of Engineering. I’m pursuing a career in product management with experience in UX design, data analysis, and frontend development. Besides a PM, I’m also a foodie, music enthusiast, and movie addict."
+    const we_bio = "N/A"
+    const rs_bio = "Ronin Sharma is a Senior Electrical & Computer Engineering major in the College of Engineering. He has prior internship experience in Data Science and Cloud Analytics, and will be an MEng student following his graduation in May 2022. Outside of school, Ronin plays badminton and tennis."
+    const nm_bio = "Neil Madhavani is a Senior Computer Science major in the College of Engineering. He has prior internship experience in Data Science and Software Engineering, and will be a Software Engineer following his graduation in May 2022. Outside of school, Neil plays golf and Squash."
+    const pb_bio = "Paul Beck is a Senior Applied Engineering Physics major in the College of Engineering. He has prior internship experience in Python and Matlab, and will be an MEng student following his graduation in May 2022. Outside of school, Paul deadlifts 580 lbs everyday."
+
     return <div>
-        <p>TESTING ABOUT US PAGE</p>
+        <h1 className="about_header">The ENGRC 3350 Team</h1>
         <div className="team_members">
-            <div className="row">
-                <Member name="Allison Hutchison" pic={ds_headshot} />
-                <Member name="Danielle Simon" pic={ds_headshot} />
-                <Member name="Nina Xie" pic={ds_headshot} />
-                <Member name="William Evans" pic={ds_headshot} />
-                {/* <img src={ds_headshot} alt="Allison Hutchison's Headshot" className="headshots" />
-                <img src={ds_headshot} alt="Danielle Simon's Headshot" className="headshots" />
-                <img src={ds_headshot} alt="Nina Xie's Headshot" className="headshots" />
-                <img src={ds_headshot} alt="William Evans's Headshot" className="headshots" /> */}
+            <div className="row row1">
+                <Member name="Allison Hutchison, PhD" pic={ah_headshot} bio={ah_bio} info="Senior Lecturer, Engineering Communications Program" />
+                <Member name="Danielle Simon" pic={ds_headshot} bio={ds_bio} info="CS '22" />
+                <Member name="Nina Xie" pic={nx_headshot} bio={nx_bio} info="ISST '22" />
+                <Member name="William Evans" pic={we_headshot} bio={we_bio} info="CS '22" />
             </div>
-            <div className="row">
-                <Member name="Ronin Sharma" pic={ds_headshot} />
-                <Member name="Neil Madhavani" pic={ds_headshot} />
-                <Member name="Paul Beck" pic={ds_headshot} />
+            <div className="row row2">
+                <Member name="Ronin Sharma" pic={rs_headshot} bio={rs_bio} info="CS '22" />
+                <Member name="Neil Madhavani" pic={nm_headshot} bio={nm_bio} info="CS '22" />
+                <Member name="Paul Beck" pic={pb_headshot} bio={pb_bio} info="CS '22" />
                 {/* <img src={ds_headshot} alt="Ronin Sharma's Headshot" className="headshots" />
-                <img src={ds_headshot} alt="Neil Madhavani's Headshot" className="headshots" />
-                <img src={ds_headshot} alt="Paul Beck's Headshot" className="headshots" /> */}
+               <img src={ds_headshot} alt="Neil Madhavani's Headshot" className="headshots" />
+               <img src={ds_headshot} alt="Paul Beck's Headshot" className="headshots" /> */}
             </div>
         </div>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,56 @@
+// import React from 'react';
+// import home_page_pic from '../home_page_pic.png';
+// import {
+//     BrowserRouter as Router,
+//     Route,
+//     Link,
+//     Routes,
+//     Button
+// } from 'react-router-dom';
+// import SkillSearch from './SkillSearch';
+// import ResumeAnalysis from './ResumeAnalysis';
+
+// function Home() {
+//     return (
+//         // <Router>
+//         <div>
+//             <div class="title-container">
+//                 <h1 id="home-title">Find your communication skills gap</h1>
+//             </div>
+//             <div class="pic_and_buttons">
+//                 <img src={home_page_pic} className="home_page_pic" alt="home page picture" />
+//                 {/* <div class="feature_buttons"> */}
+//                 <div class="skill-search-button feature-buttons">
+//                     <Link to="/skill-search" ><button type="button" class="btn btn-primary home-buttons">Skill Search</button></Link>
+//                     <p>TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123</p>
+//                 </div>
+//                 <div class="resume-analysis-button feature-buttons">
+//                     <Link to="/resume-analysis" ><button type="button" class="btn btn-primary home-buttons">Resume Analysis</button></Link>
+//                     <p>TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123</p>
+//                 </div>
+//                 {/* </div> */}
+//             </div>
+
+//             <Routes>
+//                 {/* <Route exact path='/' element={<Home />}></Route> */}
+//                 <Route exact path='/skill-search' element={<SkillSearch />}></Route>
+//                 <Route exact path='/resume-analysis' element={<ResumeAnalysis />}></Route>
+//                 {/* <Route exact path='/about' element={<About />}></Route> */}
+//             </Routes>
+
+//         </div>
+//         // </Router>
+//     )
+// }
+// export default Home;
+
 import React from 'react';
 import home_page_pic from '../home_page_pic.png';
 import {
     BrowserRouter as Router,
     Route,
     Link,
-    Routes,
-    Button
+    Routes
 } from 'react-router-dom';
 import SkillSearch from './SkillSearch';
 import ResumeAnalysis from './ResumeAnalysis';
@@ -15,21 +60,41 @@ function Home() {
         // <Router>
         <div>
             <div class="title-container">
-                <h1 id="home-title">Find your communication skills gap</h1>
+                <h1 id="home-title">Find your engineering communication skills gap</h1>
             </div>
             <div class="pic_and_buttons">
                 <img src={home_page_pic} className="home_page_pic" alt="home page picture" />
                 {/* <div class="feature_buttons"> */}
                 <div class="skill-search-button feature-buttons">
                     <Link to="/skill-search" ><button type="button" class="btn btn-primary home-buttons">Skill Search</button></Link>
-                    <p>TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123</p>
+                    <p>Search for your interested engineering job to see the most important skills required.</p>
                 </div>
                 <div class="resume-analysis-button feature-buttons">
                     <Link to="/resume-analysis" ><button type="button" class="btn btn-primary home-buttons">Resume Analysis</button></Link>
-                    <p>TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123 TESTING TESTING 123</p>
+                    <p>Evaluate your resume to improve on demonstrating your communication and collaboration skills.</p>
                 </div>
                 {/* </div> */}
             </div>
+            <div className="directions">
+                <h3>Directions</h3>
+                <p className="home_text">You can choose an interested job from our selection of 18 engineering job titles in 4 engineering job categories, to learn about the top most commonly required communication skills for that job.</p>
+                <p className="home_text">OR</p>
+                <p className="home_text">You can share with us your resume along with a job title or a job description, and we will show you an evaluation score and the most required communication skills for that job.</p>
+                <p className="home_text">Also, feel free to check out our team!</p>
+            </div>
+            <div className="purpose">
+                <h3>Purpose Statement</h3>
+                <p className="home_text">This Communication Job Skills tool aims at providing career-seeking engineering students with a way to gain a better understanding of what communication skills employers are looking for in a candidate so that users can evaluate and improve their own resumes and skill sets.</p>
+            </div>
+            <div className="background">
+                <h3>Background</h3>
+                <div className="home_text">
+                    <p>According to many engineering executives, communication skills play a critical role in both the hiring process for applicants, as well as the success and advancement of an engineer in their professional career.</p>
+                    <p>This tool consists of two main functionalities on career type skills search and resume analysis. </p>
+                    <p>Communication and collaboration skills data used for this project was scraped from 5,994 engineering job ads by an undergraduate engineering research team at Cornell University. Report for the previous research project can be found <a href="https://drive.google.com/file/d/13VSAlBt_k6wYfMksJTGsGDa3mego7EUE/view">here</a>.</p>
+                </div>
+            </div>
+
 
             <Routes>
                 {/* <Route exact path='/' element={<Home />}></Route> */}
